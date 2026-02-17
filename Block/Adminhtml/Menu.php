62b3e87441a3da43b7910b72341cb12f6297b848
Volodymyr Hryvinskyi <volodymyr@hryvinskyi.com>
2026-02-17 22:51:37 +0100

    Update menu style

--- Block/Adminhtml/Menu.php
+++ Block/Adminhtml/Menu.php
@@ -22,7 +22,11 @@ class Menu extends Template
     /**
      * Default hamburger icon SVG
      */
-    private const DEFAULT_ICON = '<svg class="hamburger-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>';
+    private const DEFAULT_ICON = '<svg class="hamburger-icon" width="14" height="14" viewBox="0 0 18 18" fill="none">
+            <line x1="1" y1="3" x2="17" y2="3" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></line>
+            <line x1="1" y1="9" x2="17" y2="9" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></line>
+            <line x1="1" y1="15" x2="17" y2="15" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></line>
+        </svg>';
 
     /**
      * @var string
