c41044419a671df8b08d0d59392e9a5fb7f94d13
Volodymyr Hryvinskyi <volodymyr@hryvinskyi.com>
2026-03-25 08:56:55 +0000

    Fix PHP 8.1 deprecation: use explicit nullable types in NoSetLayoutException

--- Model/Layout/NoSetLayoutException.php
+++ Model/Layout/NoSetLayoutException.php
@@ -23,7 +23,7 @@ class NoSetLayoutException extends LocalizedException
      * @param Exception $cause
      * @param int $code
      */
-    public function __construct(Phrase $phrase = null, Exception $cause = null, $code = 0)
+    public function __construct(?Phrase $phrase = null, ?Exception $cause = null, $code = 0)
     {
         if ($phrase === null) {
             $phrase = new Phrase('Layout was not set');
